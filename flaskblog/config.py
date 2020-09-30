7cd67bce2fd85714b71b0fd56885065e39bdde16
dhrush <dhrush97@gmail.com>
2020-09-30 23:56:24 +0530

    Changes after blueprint

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = '3e049e92525206896070b7bbffc51400'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS  = True
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskblog.models import User
+
 
 #(this class extends FlaskForm in the paranthesis)
 class RegistrationForm(FlaskForm):
@@ -50,8 +51,17 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email = email.data).first()
             if user:
                 raise ValidationError('This email already exists')
+            
 
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators = [DataRequired()])
-    submit = SubmitField('Post')
+class RequestResetForm(FlaskForm):
+     email  = StringField('Email', validators=[DataRequired(), Email()])
+     submit = SubmitField('Request Reset Password')
+     def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email address, Register First')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')            
